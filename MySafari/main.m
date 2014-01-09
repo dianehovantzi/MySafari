93d151afeee8ad24fd69c708817838cad1c4509b
Diane Hovantzi <diane.hovantzi@gmail.com>
2014-01-09 13:44:50 -0600

    Initial Commit

---
+++ MySafari/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MySafari
+//
+//  Created by Barbara Hovantzi on 1/9/14.
+//  Copyright (c) 2014 Diane Hovantzi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
